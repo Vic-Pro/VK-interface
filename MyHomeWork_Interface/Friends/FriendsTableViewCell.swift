6795f4b48ef1be05bd255b35a6387744c9bf8b4b
Victor_Pro <vikprokhorov@gmail.com>
2019-03-09 04:08:39 +0300

    Перезалил проект Отработал первую ветку (на сколько понимал)

---
+++ MyHomeWork_Interface/Friends/FriendsTableViewCell.swift
@@ -0,0 +1,26 @@
+//
+//  FriendsTableViewCell.swift
+//  MyHomeWork_Interface
+//
+//  Created by User on 09.03.2019.
+//  Copyright © 2019 User. All rights reserved.
+//
+
+import UIKit
+
+class FriendsTableViewCell: UITableViewCell {
+
+    @IBOutlet weak var FriendsName: UILabel!
+    
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
---
+++ MyHomeWork_Interface/FriendsPhoto/PhotoCollectionViewCell.swift
@@ -0,0 +1,16 @@
+//
+//  PhotoCollectionViewCell.swift
+//  MyHomeWork_Interface
+//
+//  Created by User on 09.03.2019.
+//  Copyright © 2019 User. All rights reserved.
+//
+
+import UIKit
+
+class PhotoCollectionViewCell: UICollectionViewCell {
+    
+    @IBOutlet weak var Photo: UIImageView!
+    @IBOutlet weak var Name: UILabel!
+    
+}
