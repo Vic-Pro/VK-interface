26bb4dbb93c0fa4bf8ba2de02790396524846935
Victor_Pro <vikprokhorov@gmail.com>
2019-05-26 20:16:07 +0300

    Раскоментил ошибки

---
+++ MyHomeWork_Interface/Profile/ProfileAvatarView.swift
@@ -0,0 +1,21 @@
+//
+//  ProfileAvatarView.swift
+//  MyHomeWork_Interface
+//
+//  Created by User on 26.05.2019.
+//  Copyright © 2019 User. All rights reserved.
+//
+
+import UIKit
+
+class ProfileAvatarView: UIView {
+
+    /*
+    // Only override draw() if you perform custom drawing.
+    // An empty implementation adversely affects performance during animation.
+    override func draw(_ rect: CGRect) {
+        // Drawing code
+    }
+    */
+
+}
